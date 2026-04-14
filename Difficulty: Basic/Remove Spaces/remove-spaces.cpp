8cb8a69ebba74bab92234e4dbf959d37e2991550
Aniket Saha <141211011+Aniket-a14@users.noreply.github.com>
2026-04-14 20:16:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove Spaces/remove-spaces.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    string removeSpaces(string& s) {
+        // code here
+        string ans = "";
+        
+        for(int i=0;i<s.size();i++){
+            if(s[i]==' ') continue;
+            ans+=s[i];
+        }
+        
+        return ans;
+    }
+};
